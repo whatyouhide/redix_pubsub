11dc8640fc9204ab84ccac5d3605b47a19216e4b
Andrea Leopardi <an.leopardi@gmail.com>
2016-06-26 20:36:17 +0200

    Bump version to v0.1.0 and prepare for the first release

--- mix.exs
+++ mix.exs
@@ -1,14 +1,24 @@
 defmodule RedixPubsub.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project() do
     [app: :redix_pubsub,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
-     aliases: [foo: fn(args) -> IO.puts("args: #{inspect args}") end]]
+     package: [maintainers: ["Andrea Leopardi"],
+               licenses: ["MIT"],
+               links: %{"GitHub" => "https://github.com/whatyouhide/redix_pubsub"}],
+     description: "Elixir library for using Redis Pub/Sub features (built on top of Redix)",
+     name: "Redix.PubSub",
+     docs: [main: "Redix.PubSub",
+            source_ref: "v#{@version}",
+            source_url: "https://github.com/whatyouhide/redix_pubsub",
+            extras: "README.md"]]
   end
 
   def application() do
